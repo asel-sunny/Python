bb0c6c95f8afca21a3f1a016d1f4e3036aa98106
asel-sunny <150486322+asel-sunny@users.noreply.github.com>
2023-11-26 13:51:46 -0600

    Update task2.py

--- homeworks/session9/task2.py
+++ homeworks/session9/task2.py
@@ -3,12 +3,15 @@ int2 = int(input("Enter the second number: "))
 
 operation = input("Enter the operation (+, -, *, /): ")
 
-
-if operation == "+":
-    print(int1 + int2)
-elif operation == "-":
-    print(int1 - int2)
-elif operation == "*":
-    print(int1 * int2)
-elif operation == "/":
-    print(int1 / int2)
+if operation != "/" or int2 != 0:
+   for i in operation:
+        if i == "+":
+            print(int1 + int2)
+        elif i == "-":
+            print(int1 - int2)
+        elif i == "*":
+            print(int1 * int2)
+        elif i == "/":
+            print(int1 / int2)
+else:
+    print("You cant divide by 0")
