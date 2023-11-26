2a3bba1462edf4d86a2065224fce2e46bd8a9cfe
asel-sunny <150486322+asel-sunny@users.noreply.github.com>
2023-11-26 14:31:21 -0600

    Update task2.py

--- homeworks/session8/task2.py
+++ homeworks/session8/task2.py
@@ -3,12 +3,16 @@ lst = [num for num in range(51) if num %2 == 0 ]
 
 print("Sum of all even numbers is: ",sum(lst))
 
-lst2 = [num for num in range(51) if num %2 != 0]
+lst2 = [num for num in range(51) if num %2 != 0 ]
 
-print("Product of all odd numbers is: ", sum(lst2))
+result = 1
+for x in lst2:
+    result = result * x
+print("Product of all odd numbers is: ", result)
 
-if [ sum(lst) > sum(lst2) ]:
-    print("Even numbers are greather than odd")
+
+if  sum(lst) > result :
+    print("Even numbers are greater than odd")
 else:
     print("Odd numbers are greater than even")
 
